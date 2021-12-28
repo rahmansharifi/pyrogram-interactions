4eec28651c72e24d8256b80ac4e60858721a01fd
Rahman Sharifi <rahmansharifi79@gmail.com>
2021-12-28 06:00:32 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,28 @@
+import os
+import time
+
+from pyrogram import Client, filters,idle
+from pyrogram.raw.functions.account import UpdateStatus
+
+os.chdir(os.path.dirname(__file__))
+
+app = Client(
+    session_name = 'bot',
+    api_id = 0,
+    api_hash = '',
+    app_version = '1.0',
+    device_model = 'github:rahmansharifi/pyrogram-interactions',
+    system_version = '1.0',
+)
+
+@app.on_message(filters.command('ping'))
+def _(Client, Message):
+    Client.read_history(Message.chat.id)
+    Client.send_chat_action(Message.chat.id,'typing')
+    time.sleep(1)
+    Message.reply('Userbot works fine!')
+
+app.start()
+app.send(UpdateStatus(offline=False))
+idle()
+app.stop()
